d692bc672af5c786944b0439b3358753fdce706a
goutham2996 <gautamspikee@gmail.com>
2024-04-26 13:02:20 +0530

    Updated assertions

--- InvokingBrowser.py
+++ InvokingBrowser.py
@@ -2,10 +2,13 @@ import time
 
 from selenium import webdriver
 
-driver= webdriver.Firefox()
+driver= webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com")
 driver.maximize_window()
 print(driver.title)
 print(driver.current_url)
+our_url = "https://rahulshettyacademy.com/"
+fetched_url = driver.current_url
+
 
 time.sleep(2)
